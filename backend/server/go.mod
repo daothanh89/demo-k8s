0aa0cf3570409e1386f9326ee289bd0a6869d876
daothanh89 <thanhdao.junctionxhanoi@gmail.com>
2020-08-22 16:46:36 +0700

    add server

---
+++ backend/server/go.mod
@@ -0,0 +1,3 @@
+module demo-k8s-server
+
+go 1.15
---
+++ backend/server/static/build/precache-manifest.162d3c3255fe67cd833d25e7e5f95365.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77366c05371e515425e2a335579afa72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199f4f17d6e8b4e5c2b3",
+    "url": "/static/js/2.1345eff5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1345eff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33d98dfb52768a5dad4",
+    "url": "/static/js/main.951e2cf7.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
